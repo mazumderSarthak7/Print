cb33ae97b485fe48e857e936c372dbe9bbfe2bfc
mazumderSarthak7 <87946829+mazumderSarthak7@users.noreply.github.com>
2021-07-26 22:44:47 +0530

    Add files via upload

---
+++ greatest number.py
@@ -0,0 +1,13 @@
+
+x=int(input("Enter the 1st Number: "))
+y=int(input("Enter the 2nd Number: "))
+z=int(input("Enter the 3nd Number: "))
+print("the greatest number is ")
+if x>y & x>z:
+    print(x)
+elif y>x & y>z:
+    print(y)
+else:
+    print(z)
+
+
